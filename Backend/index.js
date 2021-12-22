f24a014d1f193fb8303b023d8d7c28bb9f85737b
Leandro <leandroquiroga9514@gmail.com>
2021-12-22 20:57:14 -0300

    Backend APP

--- Backend/index.js
+++ Backend/index.js
@@ -2,6 +2,7 @@ import Express  from "express";
 import connectingDB from './database/db.js'
 import router from './router/index.js';
 import dotenv from 'dotenv';
+import cors from 'cors';
 
 // Enable environment variables
 dotenv.config({ path: 'variables.env' });
@@ -9,6 +10,16 @@ dotenv.config({ path: 'variables.env' });
 // Creater Server
 const server = Express(); 
 
+// Enable cors
+// const whitelist = ['http://localhost:3000'];
+// const corsOptions = {
+//     origin: (origin, callback) => {
+//         const existe = whitelist.some(dominio => dominio === origin);
+//         (existe) ? callback(null, true) : callback(new Error('You are not access'))
+//     } 
+// }
+// server.use(cors(corsOptions))
+server.use(cors())
 // Connecting MongoDB
 connectingDB();
 
