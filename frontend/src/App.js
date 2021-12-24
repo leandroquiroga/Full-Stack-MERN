0b608bf1c0e81519916cb71f03053ed7a831c654
Leandro <leandroquiroga9514@gmail.com>
2021-12-23 22:21:48 -0300

    Frontend App

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,23 +10,36 @@ import Main from './components/Main/Main';
 function App() {
 
   const [list_Task, saveList_Task] = useState([]);
+  const [stateRefresh, changeStateRefresh] = useState(true)
 
   useEffect(() => {
-    const api = () => {
-      (clientAxios.get('/task'))
-      .then(response => saveList_Task(response.data))
-      .catch(error => console.log(error))
+    if (stateRefresh) { 
+      const api = () => {
+        clientAxios.get('/task')
+        .then(response => {
+            saveList_Task(response.data)
+            
+            // disabled stateRefresh
+            changeStateRefresh(false)
+          }) 
+          .catch(error => console.log(error))
+      }
+      api()
     }
-    api()
-  }, [])
+  }, [stateRefresh])
   return (
     <section className="App text-white">
       <Header 
         title='Todo List'
+        nameInput='Ingrese su nombre'
+        taksInput='Ingrese su tarea'
+        dateInput='Ingrese dia de finalizacion'
+        hoursInput='Ingrese hora de entrega' 
+        btnText='Crear nueva tarea'
+        changeStateRefresh={changeStateRefresh}
       /> 
       <Main
-        tasks={list_Task} 
-          
+        tasks={list_Task}
       />
       {/* <Footer />  */}
     </section>
